82b421bc7d3b5842f40f93f88fd9f4f5c740dd68
milindhg <milindhg@gmail.com>
2016-06-08 17:09:25 -0400

    some comments added

--- src/AdapterDesignPattern/AdapterTester.java
+++ src/AdapterDesignPattern/AdapterTester.java
@@ -1,5 +1,26 @@
 package AdapterDesignPattern;
 
+/**
+ * 
+ * @Adapter_Design_Pattern: // Our source here is turkey and we want to develop
+ *                          an adapter which makes turkey behave like duck or
+ *                          appear like duck.
+ * 
+ *                          2 ways of doing it -
+ * 
+ *                          1. Class Adapter Implementation i.e. The adapter
+ *                          extends the source class i.e. Turkey and implements
+ *                          Duck interface.
+ * 
+ *                          2. Object Adapter Implementation i.e. the adapter
+ *                          simple implements the Duck interface. Here there is
+ *                          also composition i.e. the Adapter class is composed
+ *                          of the source object i.e. Turkey object.
+ * 
+ * 
+ * @author Milind
+ *
+ */
 public class AdapterTester {
 	public static void main(String[] args) {
 		Duck mallard = new MallardDuck();
--- src/AdapterDesignPattern/DuckAdapter.java
+++ src/AdapterDesignPattern/DuckAdapter.java
@@ -1,12 +1,23 @@
 package AdapterDesignPattern;
 
+/**
+ * @Object_Adapter_Implementation: Object Adapter Implementation i.e. the
+ *                                 adapter simple implements the Duck interface.
+ *                                 Here there is also composition i.e. the
+ *                                 Adapter class is composed of the source
+ *                                 object i.e. Turkey object.
+ * 
+ * @author Milind
+ *
+ */
 public class DuckAdapter implements Duck {
 
 	Turkey t;
-	
+
 	public DuckAdapter(Turkey t) {
-		this.t=t;
+		this.t = t;
 	}
+
 	@Override
 	public void quack() {
 		System.out.println("Duck Quack");
